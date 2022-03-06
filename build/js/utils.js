12114b018a6ef007adb65a3b967c67255b7a990e
maya15102 <alv20034@byui.edu>
2022-03-05 21:44:06 -0800

    L04 Individual Activity 3

--- build/js/utils.js
+++ build/js/utils.js
@@ -13,3 +13,11 @@ export function setClick(e, t) {
   }),
     qs(e).addEventListener("click", t);
 }
+
+export function renderListWithTemplate(template, parent, list, callback) {
+  list.forEach(item => {
+    const clone = template.content.cloneNode(true);
+    const templateWithData = callback(clone, item);
+    parent.appendChild(templateWithData);
+  })
+}
